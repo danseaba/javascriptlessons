940ec0065c0139a383eb8a2e885a8f78bb0bf347
danseaba <worldclassbrains@gmail.com>
2022-02-07 13:36:46 +0100

    variables and data types

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
-console.log("Hello")
+// Three Keyword for declaring variables
+
+var firstname = "";
+
+firstname = {
+    name: "First Name",
+}
+console.log(firstname);
+
+let isLoggedin = true;
+isLoggedin = false;
+
+const _ = new Date();
+
+
+// Data Types
+
+let Arrays = [1,5,5, 6, 6, 45, 65] // Arrays
+let name = "timi" // String
+let isLoggedinA = false // True or false
+let person = {
+    name:"first"
+} // Object
+let age = 12; //Number
+
+let user = undefined; // Undefined
+let users = null; // Null- Nothing in attached
+let newSymbols = Symbol(Arrays) // List in Python
+
+let newSet = [...new Set(Arrays)]; // Unique Items
+
+console.log( newSet)
+
+
+
